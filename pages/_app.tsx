adf4a1bd89610a25be8125e121e443a134fa6ea7
Cha'sMacAir <chayh311@gmail.com>
2020-10-04 17:47:30 +0900

    fix: Import aos in useEffect

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,11 @@
 import '../tailwind.css'
 import { useEffect } from 'react'
-import AOS from 'aos';
+// import AOS from 'aos';
 import 'aos/dist/aos.css';
 
 export default function MyApp({ Component, pageProps }) {
   useEffect(() => {
+    const AOS = require('aos');
     AOS.init({
       duration : 1000
     });
